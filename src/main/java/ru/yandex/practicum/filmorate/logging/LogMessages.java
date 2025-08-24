a00daff2ddc2c22b3e4a0e19293eb5c6f15f6ff6
Nikita Rovnin <Nikita.Rovnin@gmail.com>
2025-08-24 12:01:09 +0700

    Править логи

--- src/main/java/ru/yandex/practicum/filmorate/logging/LogMessages.java
+++ src/main/java/ru/yandex/practicum/filmorate/logging/LogMessages.java
@@ -2,32 +2,32 @@ package ru.yandex.practicum.filmorate.logging;
 
 public class LogMessages {
     // Trace
-    public static final String FILM_ADD = "Начало метода addFilm, на входе фильм: {}"; // film
-    public static final String FILM_UPDATE = "Начало метода updateFilm, на входе фильм: {}"; // film
-    public static final String FILM_DELETE = "Начало метода deleteFilm, на входе фильм с id: {}"; // id
-    public static final String USER_ADD = "Начало метода registerUser, на входе пользователь: {}"; // user
-    public static final String USER_UPDATE = "Начало метода updateUser, на входе пользователь: {}"; // user
-    public static final String USER_DELETE = "Начало метода deleteUser, на входе пользователь с id: {}"; // id
+    public static final String FILM_ADD = "Начало метода addFilm, film: {}"; // film
+    public static final String FILM_UPDATE = "Начало метода updateFilm, film: {}"; // film
+    public static final String FILM_DELETE = "Начало метода deleteFilm, id: {}"; // id
+    public static final String USER_ADD = "Начало метода registerUser, user: {}"; // user
+    public static final String USER_UPDATE = "Начало метода updateUser, user: {}"; // user
+    public static final String USER_DELETE = "Начало метода deleteUser, id: {}"; // id
 
     // Info
-    public static final String FILM_SAVE_SUCCESS = "Фильм успешно сохранен и возвращен: {}"; // film
-    public static final String FILM_UPDATE_SUCCESS = "Фильм успешно обновлен и возвращен: {}"; // film
-    public static final String FILM_DELETE_SUCCESS = "Фильм успешно удален и возвращен: {}"; // film
-    public static final String USER_SAVE_SUCCESS = "Пользователь успешно сохранен и возвращен: {}"; // user
-    public static final String USER_UPDATE_SUCCESS = "Пользователь успешно обновлен и возвращен: {}"; // user
-    public static final String USER_DELETE_SUCCESS = "Пользователь успешно удален и возвращен: {}"; // user
+    public static final String FILM_SAVE_SUCCESS = "Фильм успешно сохранен, film: {}"; // film
+    public static final String FILM_UPDATE_SUCCESS = "Фильм успешно обновлен, film: {}"; // film
+    public static final String FILM_DELETE_SUCCESS = "Фильм успешно удален, film: {}"; // film
+    public static final String USER_SAVE_SUCCESS = "Пользователь успешно сохранен, user: {}"; // user
+    public static final String USER_UPDATE_SUCCESS = "Пользователь успешно обновлен, user: {}"; // user
+    public static final String USER_DELETE_SUCCESS = "Пользователь успешно удален, user: {}"; // user
 
     // Debug
-    public static final String FILM_SAVE_STARTED = "Начинается сохранение фильма: {}"; // film
-    public static final String FILM_UPDATE_STARTED = "Начинается обновление фильма с id: {}"; // id
-    public static final String FILM_DELETE_STARTED = "Начинается удаление фильма с id: {}"; // id
-    public static final String USER_SAVE_STARTED = "Начинается сохранение пользователя: {}"; // user
-    public static final String USER_UPDATE_STARTED = "Начинается обновление пользователя с id: {}"; // id
-    public static final String USER_DELETE_STARTED = "Начинается удаление пользователя с id: {}"; // id
+    public static final String FILM_SAVE_STARTED = "Сохранение фильма: {}"; // film
+    public static final String FILM_UPDATE_STARTED = "Обновление фильма с id: {}"; // id
+    public static final String FILM_DELETE_STARTED = "Удаление фильма с id: {}"; // id
+    public static final String USER_SAVE_STARTED = "Сохранение пользователя: {}"; // user
+    public static final String USER_UPDATE_STARTED = "Обновление пользователя с id: {}"; // id
+    public static final String USER_DELETE_STARTED = "Удаление пользователя с id: {}"; // id
 
     // Warn
-    public static final String FILM_UPDATE_NOT_FOUND = "Попытка обновить данные фильма, фильм не найден, id: {}"; // id
+    public static final String FILM_UPDATE_NOT_FOUND = "Попытка обновить фильм, фильм не найден, id: {}"; // id
     public static final String FILM_DELETE_NOT_FOUND = "Попытка удалить фильм, фильм не найден, id: {}"; // id
-    public static final String USER_UPDATE_NOT_FOUND = "Попытка обновить данные пользователя, пользователь не найден, id: {}"; // id
-    public static final String USER_DELETE_NOT_FOUND = "Попытка удалить Пользователя, пользователь не найден, id: {}"; // id
+    public static final String USER_UPDATE_NOT_FOUND = "Попытка обновить пользователя, пользователь не найден, id: {}"; // id
+    public static final String USER_DELETE_NOT_FOUND = "Попытка удалить пользователя, пользователь не найден, id: {}"; // id
 }
